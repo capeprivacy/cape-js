51e14351e0fe60f0f52c1bc922d1face2586ee61
Devin Schulz <devin@devinschulz.com>
2022-06-14 12:10:48 -0400

    Fix rename typos

--- packages/isomorphic/src/parse-attestation-document-node.ts
+++ packages/isomorphic/src/parse-attestation-document-node.ts
@@ -3,15 +3,17 @@ import { decodeAllSync } from 'cbor';
 import type { AttestationDocument } from '@cape/types';
 
 /**
- * Parses an isomorphic document and returns the decoded payload.
- * @param document The isomorphic document to parse represented as a buffer-like object.
+ * Parses an attestation document and returns the decoded payload.
+ *
+ * @param document The attestation document base64 encoded.
+ * @returns The attestation document.
  */
 export const parseAttestationDocument = (document: string): AttestationDocument => {
   const payloadArray = decodeAllSync(Buffer.from(document, 'base64'), {
     preferWeb: true, // Uses Uint8Array over Buffer needed for Tink
   })[0];
   if (!Array.isArray(payloadArray) || payloadArray.length !== 4) {
-    throw new Error('Invalid isomorphic document');
+    throw new Error('Invalid attestation document');
   }
   return decodeAllSync(payloadArray[2], {
     preferWeb: true,
