fa5c3d8e1f60717f0042d1291adaf86b7aebf4fd
Devin Schulz <devin@devinschulz.com>
2022-07-20 16:08:51 -0400

    Fix webcrypto error

--- packages/isomorphic/src/parse-attestation-document-node.spec.ts
+++ packages/isomorphic/src/parse-attestation-document-node.spec.ts
@@ -1,14 +1,16 @@
 import { verifySignature } from './parse-attestation-document-node';
+import * as pkijs from 'pkijs';
 
 import * as crypto from 'crypto';
 
 beforeEach(() => {
-  global.crypto = crypto.webcrypto;
+  const name = 'nodeEngine';
+  pkijs.setEngine(name, new pkijs.CryptoEngine({ name, crypto: crypto.webcrypto }));
 });
 
 describe('verify-sig.node', () => {
   it('should do a thing', async () => {
     const str = await verifySignature('dGVzdA==');
-    expect(str).toBe('test');
+    expect(str).toBe(true);
   });
 });
--- packages/isomorphic/src/parse-attestation-document-node.ts
+++ packages/isomorphic/src/parse-attestation-document-node.ts
@@ -1,12 +1,7 @@
-import 'cbor-rn-prereqs'; // Fixes TextDecoder not found issue and must be imported before cbor
-import { decodeAllSync } from 'cbor';
 import type { AttestationDocument } from '@capeprivacy/types';
-import { pki, asn1, util } from 'node-forge';
+import { decodeAllSync } from 'cbor';
+import 'cbor-rn-prereqs'; // Fixes TextDecoder not found issue and must be imported before cbor
 import { Certificate } from 'pkijs';
-import { ec } from 'elliptic';
-import { base64Decode } from './base64-decode-node';
-import { webcrypto } from 'crypto';
-import { setEngine, CryptoEngine } from 'pkijs';
 
 /**
  * Parses an attestation document and returns the decoded payload.
@@ -28,19 +23,19 @@ export const parseAttestationDocument = (document: string): AttestationDocument 
 };
 
 export const verifySignature = async (publicKey: string): Promise<boolean> => {
-  var pem =
+  const pem =
     '-----BEGIN CERTIFICATE-----\nMIICezCCAgGgAwIBAgIQAYH+dIT3SiQAAAAAYtCAjzAKBggqhkjOPQQDAzCBjjEL\nMAkGA1UEBhMCVVMxEzARBgNVBAgMCldhc2hpbmd0b24xEDAOBgNVBAcMB1NlYXR0\nbGUxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMTkwNwYDVQQDDDBpLTAy\nOWUwZWFjYjY1MjY3Y2FhLnVzLWVhc3QtMi5hd3Mubml0cm8tZW5jbGF2ZXMwHhcN\nMjIwNzE0MjA0NjA0WhcNMjIwNzE0MjM0NjA3WjCBkzELMAkGA1UEBhMCVVMxEzAR\nBgNVBAgMCldhc2hpbmd0b24xEDAOBgNVBAcMB1NlYXR0bGUxDzANBgNVBAoMBkFt\nYXpvbjEMMAoGA1UECwwDQVdTMT4wPAYDVQQDDDVpLTAyOWUwZWFjYjY1MjY3Y2Fh\nLWVuYzAxODFmZTc0ODRmNzRhMjQudXMtZWFzdC0yLmF3czB2MBAGByqGSM49AgEG\nBSuBBAAiA2IABAV56iqfGfjdMyXc5cKI47J9gIdxkkJyjRATnKfVSx+4RYO53Sqa\nb6BeljMJnI5DdC+gubDLqoIzFCSqxZWQ55FeDytA2PP3YaOH/KEOSoCe1Yi5sHWd\nAmBCuBPvGPZTJ6MdMBswDAYDVR0TAQH/BAIwADALBgNVHQ8EBAMCBsAwCgYIKoZI\nzj0EAwMDaAAwZQIwEQ+tuK1NHDAlS3rtWC0kieUZoZTOMaEdD7v2lQnp+zgJu6mU\n904Cx0QaI+kKzy0bAjEAne90J4+NEngbmOi8Sj9ebA+eKQ45/ntoSzdT9ArQuVXq\n9PETUpqM4vP+K6Rcp4BW\n-----END CERTIFICATE-----\n';
-  var der =
+  const der =
     'MIICezCCAgGgAwIBAgIQAYH+dIT3SiQAAAAAYtCAjzAKBggqhkjOPQQDAzCBjjELMAkGA1UEBhMCVVMxEzARBgNVBAgMCldhc2hpbmd0b24xEDAOBgNVBAcMB1NlYXR0bGUxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMTkwNwYDVQQDDDBpLTAyOWUwZWFjYjY1MjY3Y2FhLnVzLWVhc3QtMi5hd3Mubml0cm8tZW5jbGF2ZXMwHhcNMjIwNzE0MjA0NjA0WhcNMjIwNzE0MjM0NjA3WjCBkzELMAkGA1UEBhMCVVMxEzARBgNVBAgMCldhc2hpbmd0b24xEDAOBgNVBAcMB1NlYXR0bGUxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMT4wPAYDVQQDDDVpLTAyOWUwZWFjYjY1MjY3Y2FhLWVuYzAxODFmZTc0ODRmNzRhMjQudXMtZWFzdC0yLmF3czB2MBAGByqGSM49AgEGBSuBBAAiA2IABAV56iqfGfjdMyXc5cKI47J9gIdxkkJyjRATnKfVSx+4RYO53Sqab6BeljMJnI5DdC+gubDLqoIzFCSqxZWQ55FeDytA2PP3YaOH/KEOSoCe1Yi5sHWdAmBCuBPvGPZTJ6MdMBswDAYDVR0TAQH/BAIwADALBgNVHQ8EBAMCBsAwCgYIKoZIzj0EAwMDaAAwZQIwEQ+tuK1NHDAlS3rtWC0kieUZoZTOMaEdD7v2lQnp+zgJu6mU904Cx0QaI+kKzy0bAjEAne90J4+NEngbmOi8Sj9ebA+eKQ45/ntoSzdT9ArQuVXq9PETUpqM4vP+K6Rcp4BW';
 
-  var derBuf = Buffer.from(der, 'base64');
+  const derBuf = Buffer.from(der, 'base64');
   // var firstPub = keys.publicKey
   // var firstAsn1 = pki.publicKeyToAsn1(firstPub)
   // var derBuf = util.decode64(derb64)
 
-  var cert = Certificate.fromBER(derBuf);
-  var pub = await cert.getPublicKey();
-  pub.console.log(pub);
+  const cert = Certificate.fromBER(derBuf);
+  const pub = await cert.getPublicKey();
+  console.log(pub);
   // var der = firstAsn1.type
   //var pub = pki.certificateFromPem(pem)
 
