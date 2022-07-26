ed5d8e5f34602e04f859a187bc4006a5c0f6e53f
Justin Patriquin <justinpatriquin@gmail.com>
2022-07-26 16:20:48 -0300

    Verify root certificate
    For node it download the root cert from the proper location.
    For browser it embeds the cert in js and can be auditted by the user.

--- packages/isomorphic/src/index.node.ts
+++ packages/isomorphic/src/index.node.ts
@@ -1,4 +1,4 @@
 export * from './base64-decode-node';
 export * from './get-bytes-node';
 export * from './parse-attestation-document-node';
-export * from './verify-cert-chain';
+export * from './verify-cert-chain-node';
