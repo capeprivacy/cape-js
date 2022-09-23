c24b90f8c0324a20f841fea24d21ee5815939205
Eric <eric@capeprivacy.com>
2022-09-23 16:20:16 -0400

    add crypto-js and encryption helpers

--- packages/cape/src/encrypt.ts
+++ packages/cape/src/encrypt.ts
@@ -1,4 +1,6 @@
 import { Aead, CipherSuite, Kdf, Kem } from 'hpke-js';
+import { randomBytes, publicEncrypt } from 'crypto';
+import { CryptoJS } from 'cryptojs';
 
 interface EncryptResponse {
   cipherText: Uint8Array;
@@ -25,4 +27,20 @@ export async function encrypt(plainText: Uint8Array, publicKey: Uint8Array): Pro
   const { ct, enc } = await suite.seal({ recipientPublicKey: rkp }, plainText);
   // Convert both the ct and enc from an ArrayBuffer to Uint8Array's.
   return { cipherText: new Uint8Array(ct), encapsulatedKey: new Uint8Array(enc) };
+}
+
+export async function aesEncrypt(plainText: Uint8Array): Promise<EncryptResponse> {
+  // Generate a new key
+  const key = randomBytes(32);
+
+  const encrypted = CryptoJS.AES.encrypt(plainText, key);
+  return { cipherText: new Uint8Array(encrypted), encapsulatedKey: new Uint8Array(key) };
+}
+
+export async function rsaEncrypt(plainText: Uint8Array, key: Uint8Array): Promise<Uint8Array> {
+  const keyBuffer = Buffer.from(key);
+  const buffer = Buffer.from(plainText);
+  const encrypted = publicEncrypt(keyBuffer, buffer);
+  const cipherText = new Uint8Array(encrypted);
+  return cipherText;
 }
