4b4dbbf4cd58aa610b002960e5578b376bf33202
Eric <eric@capeprivacy.com>
2022-09-24 00:30:09 -0400

    add tests to encrypt

--- packages/cape/src/encrypt.spec.ts
+++ packages/cape/src/encrypt.spec.ts
@@ -1,4 +1,4 @@
-import { encrypt, suite } from './encrypt';
+import { encrypt, aesEncrypt, rsaEncrypt, suite } from './encrypt';
 import { TextDecoder, TextEncoder } from 'util';
 import type { XCryptoKey } from 'hpke-js/types/src/xCryptoKey';
 
@@ -12,5 +12,19 @@ describe('encrypt', () => {
     const { cipherText, encapsulatedKey } = await encrypt(encoder.encode(text), (keyPair.publicKey as XCryptoKey).key);
     const plainText = await suite.open({ recipientKey: keyPair, enc: encapsulatedKey }, cipherText);
     expect(decoder.decode(plainText)).toBe(text);
+  });
+
+  test('encrypt and decrypt using AES', async () => {
+    const text = 'what is rsa';
+    const encrypted = await aesEncrypt(encoder.encode(text));
+
+    const key = decoder.decode(encrypted.encapsulatedKey);
+    // We parse the byte array to string then use the Crypto library encoding to turn it to WordArray.
+    const parsed_iv = CryptoJS.enc.Utf8.parse(decoder.decode(encrypted.cipherText.slice(0, 12)));
+
+    const cipherText = decoder.decode(encrypted.cipherText.slice(12, encrypted.cipherText.length));
+
+    const decrypted = CryptoJS.AES.decrypt(cipherText, key, { iv: parsed_iv });
+    console.log('decrypted message', decrypted);
   });
 });
--- packages/cape/src/encrypt.ts
+++ packages/cape/src/encrypt.ts
@@ -1,7 +1,9 @@
+import { TextEncoder } from 'util';
+import { TextDecoder } from '@capeprivacy/isomorphic';
 import { Aead, CipherSuite, Kdf, Kem } from 'hpke-js';
 import { randomBytes, publicEncrypt } from 'crypto';
-import { CryptoJS } from 'cryptojs';
-
+import CryptoJS from 'crypto-js';
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1/build/components/lib-typedarrays.js"></script>;
 interface EncryptResponse {
   cipherText: Uint8Array;
   encapsulatedKey: Uint8Array;
@@ -29,12 +31,33 @@ export async function encrypt(plainText: Uint8Array, publicKey: Uint8Array): Pro
   return { cipherText: new Uint8Array(ct), encapsulatedKey: new Uint8Array(enc) };
 }
 
+/**
+ * Encrypts the given input using a generated public key.
+ *
+ * @note this function does not base64 encode the ciphertext and just returns a uint8Array.
+ *
+ * @param plainText The plain text input to encrypt.
+ */
 export async function aesEncrypt(plainText: Uint8Array): Promise<EncryptResponse> {
+  // CryptoJS doesn't provide encoding to string so we use TextDecoder.
+  // Note that this would limit some special characters in what we want to encrypt
+  // because we don't use base64 encoding.
+  const plainTextString = arrayToBase64_(plainText);
   // Generate a new key
   const key = randomBytes(32);
+  const keyString = arrayToBase64_(key);
 
-  const encrypted = CryptoJS.AES.encrypt(plainText, key);
-  return { cipherText: new Uint8Array(encrypted), encapsulatedKey: new Uint8Array(key) };
+  // aesGCM uses 12 byte nonce.
+  const iv = CryptoJS.lib.WordArray.random(16);
+
+  console.log('iv', keyString);
+  // Library encrypt call expects everything in string format.
+  const encrypted = CryptoJS.AES.encrypt(plainTextString, keyString, { iv: iv });
+  const ivString = toString(iv);
+  console.log('iv', ivString);
+  const ciphertextString = toString(encrypted.ciphertext);
+  const returnVal = new TextEncoder().encode(ivString + ciphertextString);
+  return { cipherText: returnVal, encapsulatedKey: new Uint8Array(key) };
 }
 
 export async function rsaEncrypt(plainText: Uint8Array, key: Uint8Array): Promise<Uint8Array> {
@@ -43,4 +66,20 @@ export async function rsaEncrypt(plainText: Uint8Array, key: Uint8Array): Promis
   const encrypted = publicEncrypt(keyBuffer, buffer);
   const cipherText = new Uint8Array(encrypted);
   return cipherText;
+}
+
+function toString(words: CryptoJS.lib.WordArray) {
+  return CryptoJS.enc.Base64.stringify(words);
+}
+
+/**
+ * Helper function to convert everything to base64 string
+ */
+function arrayToBase64_(array: Uint8Array) {
+  var binary = '';
+  var len = array.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(array[i]);
+  }
+  return window.btoa(binary);
 }
