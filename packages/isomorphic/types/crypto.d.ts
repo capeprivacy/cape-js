574ebc5f56c2cd823e7bae5891362dd2706718e6
Justin Patriquin <justinpatriquin@gmail.com>
2023-01-27 10:54:09 -0400

    remove unneeded file

--- packages/isomorphic/types/crypto.d.ts
+++
@@ -1,8 +0,0 @@
-// declare module 'crypto' {
-//   namespace webcrypto {
-//     const subtle: SubtleCrypto;
-//     const getRandomValues: <T extends ArrayBufferView | null>(array: T) => T;
-//     /** Available only in secure contexts. */
-//     const randomUUID: () => string;
-//   }
-// }
